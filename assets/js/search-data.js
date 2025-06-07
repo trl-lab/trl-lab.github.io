6c759690e61e63f83f71526ee58d2c0dc312e9ff
madelonhulsebos <madelonhulsebos@users.noreply.github.com>
2025-06-07 12:03:25 +0000

    Deploying to gh-pages from @ trl-lab/trl-lab.github.io@c667a9edc35ccc2fea11c9f560ce590cdef81986 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -17,7 +17,7 @@ ninja.data = [{
             window.location.href = "/people/";
           },
         },{id: "nav-publications",
-          title: "publications",
+          title: "Publications",
           description: "publications by categories in reversed chronological order.",
           section: "Navigation",
           handler: () => {
